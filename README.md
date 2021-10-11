ab0e97ad9b6dfa83b18369a01d6732c127bb25a4
chriamue <chriamue@gmail.com>
2021-10-11 10:03:59 +0100

    :tada: implemented lib

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # gpt-j-ru
 GPT-J client lib
+
+## quickstart
+
+```sh
+cargo test -- --nocapture
+```
+
+```
+running 1 test
+ClassifyResponse { sequence: "In a shocking finding, scientists discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.", labels: ["positive", "neutral", "negative"], scores: [0.6839565, 0.25236782, 0.06367566] }
+test tests::test_classify ... ok
+
+test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.87s
+```
+
+## Thanks
+
+Thanks go to vicgalle for providing https://github.com/vicgalle/gpt-j-api and the api service
+http://api.vicgalle.net:5000/docs.
---
+++ src/lib.rs
@@ -0,0 +1,98 @@
+use reqwest::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ClassifyResponse {
+    sequence: String,
+    labels: Vec<String>,
+    scores: Vec<f32>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GenerateResponse {
+    model: String,
+    compute_time: f32,
+    text: String,
+    prompt: String,
+}
+
+pub struct GPT {
+    pub url: String,
+}
+
+impl GPT {
+    pub async fn classify(
+        &self,
+        labels: &Vec<String>,
+        sequence: String,
+    ) -> Result<ClassifyResponse, Error> {
+        let client = reqwest::Client::new();
+        let response = client
+            .post(format!("{}/classify", self.url))
+            .query(&[("labels", labels.join(",")), ("sequence", sequence)])
+            .send()
+            .await?;
+        let json: ClassifyResponse = response.json::<ClassifyResponse>().await?;
+        Ok(json)
+    }
+
+    pub async fn generate(
+        &self,
+        context: String,
+        token_max_length: u16,
+        temperature: f32,
+        top_p: f32,
+        stop_sequence: Option<String>,
+    ) -> Result<GenerateResponse, Error> {
+        let client = reqwest::Client::new();
+        let response = client
+            .post(format!("{}/generate", self.url))
+            .query(&[
+                ("context", context),
+                ("token_max_length", token_max_length.to_string()),
+                ("temperature", temperature.to_string()),
+                ("top_p", top_p.to_string()),
+                ("stop_sequence", stop_sequence.unwrap_or("\"\"".to_string())),
+            ])
+            .send()
+            .await?;
+        let json: GenerateResponse = response.json::<GenerateResponse>().await?;
+        Ok(json)
+    }
+}
+
+impl Default for GPT {
+    fn default() -> Self {
+        GPT {
+            url: "http://api.vicgalle.net:5000".to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::GPT;
+
+    #[tokio::test]
+    async fn test_classify() {
+        let gpt: GPT = GPT::default();
+        let labels: Vec<String> = vec![
+            "positive".to_string(),
+            "neutral".to_string(),
+            "negative".to_string(),
+        ];
+        let sequence: String = "In a shocking finding, scientists discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.".to_string();
+        let response = gpt.classify(&labels, sequence).await.unwrap();
+        println!("{:?}", response);
+        assert_eq!(response.labels[0], "positive");
+    }
+
+    #[tokio::test]
+    async fn test_generate() {
+        let gpt: GPT = GPT::default();
+        let context = "Hello World in C++".to_string();
+        let response = gpt.generate(context, 255, 0.9, 0.9, None).await.unwrap();
+        println!("{:?}", response);
+    }
+}
